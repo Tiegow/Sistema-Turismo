1d7d668b34bfcdd86835d2477b312c335ff43625
Tiegow <88515883+Tiegow@users.noreply.github.com>
2026-06-23 17:57:51 -0300

    Primeiro commit: Estrutura inicial do Spring Boot e ConnectionFactory

---
+++ src/main/java/br/com/agencia/SistemaTurismoApplication.java
@@ -0,0 +1,13 @@
+package br.com.agencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTurismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTurismoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/agencia/config/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package br.com.agencia.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/agencia_turismo?useTimezone=true&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root"; 
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver do MySQL não encontrado.", e);
+        }
+    }
+}
