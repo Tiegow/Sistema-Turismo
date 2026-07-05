3b5969cc45ef7ad457bad30a0d6ff80017b58939
JoabUrbano <joab.uaraujo@gmail.com>
2026-07-04 23:23:38 -0300

    Integrar motorista e guia no crude de colaborador

--- src/main/java/br/com/agencia/model/Motorista.java
+++ src/main/java/br/com/agencia/model/Motorista.java
@@ -1,11 +1,14 @@
 package br.com.agencia.model;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 public class Motorista extends Colaborador {
     private Integer numeroCnh;
-    private LocalDateTime validade;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate validade;
     private List<String> categoriasCnh;
 
     public Motorista() {}
@@ -13,8 +16,8 @@ public class Motorista extends Colaborador {
     public Integer getNumeroCnh() { return numeroCnh; }
     public void setNumeroCnh(Integer numeroCnh) { this.numeroCnh = numeroCnh; }
 
-    public LocalDateTime getValidade() { return validade; }
-    public void setValidade(LocalDateTime validade) { this.validade = validade; }
+    public LocalDate getValidade() { return validade; }
+    public void setValidade(LocalDate validade) { this.validade = validade; }
 
     public List<String> getCategoriasCnh() { return categoriasCnh; }
     public void setCategoriasCnh(List<String> categoriasCnh) { this.categoriasCnh = categoriasCnh; }
